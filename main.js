982918bccb2779fa1cadbb2cbc12b1b1615ec8c8
Margulan <msbd31163@gmail.com>
2020-11-17 22:41:02 +0600

    first task

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-alert("Привет Марафоней!");
+const firstRow="мама мыла раму";
+const secondRow="собака друг человека";
+let count1=0;
+let count2=0;
+function getRow(firstRow,secondRow){
+    for(let i=0;i<firstRow.length;i++){
+        if(firstRow[i]==="а"){
+        count1++;
+        }
+    }
+    for(let k=0;k<secondRow.length;k++){
+        if(secondRow[k]==="а"){
+        count2++;
+        }
+    }
+    if(count1>count2)
+        return firstRow;
+    
+    else
+        return secondRow;
+    
+}
+
+console.log(getRow(firstRow, secondRow));
+
+
+//Задание под *
